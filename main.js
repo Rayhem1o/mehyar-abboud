9257a3dfb39e959dd4941afd4bfc186a308385c8
HMBond <hubertusmariabond@gmail.com>
2023-05-07 02:44:31 +0200

    add photos

---
+++ main.js
@@ -0,0 +1,6 @@
+window.handleReadMore = function () {
+  document.querySelector("#read-more").classList.add("hidden");
+  document
+    .querySelector("#read-more-hidden-container")
+    .classList.remove("hidden");
+};
